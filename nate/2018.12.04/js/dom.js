769b22c9bbd5140842e78daea4e9bc6cdeeb8fa6
Warnita Adams <warnita.adams.rsa@gmail.com>
2018-12-04 12:18:20 +0200

    started on manipulationg the dom

---
+++ nate/2018.12.04/js/dom.js
@@ -0,0 +1,20 @@
+// document.getElementById("demo").innerHTML= "Hello world!";
+// document.getElementById("demo").style.color= "pink";
+
+
+// function red() {
+//     document.getElementById('change_color').style.color ='red'     
+// }
+
+// function black() {
+//     document.getElementById('change_color').style.color ='black'     
+// }
+
+// function blue() {
+//     document.getElementById('change_color').style.color ='blue'     
+// }
+
+function changeColor(newColor) {
+    let btn = document.getElementById('change_color');
+    btn.style.color = newColor;
+} 
---
+++ nate/2018.12.04/js/manipulating-the-dom.js
@@ -0,0 +1,14 @@
+// document object module
+
+// modify documents after they have finished loading- add new content to a page, change the color of an element to reflect a changing state, or remove an element the user does not need anymore.
+
+// interact with your users - whether ticking a box, commenting on a blog post, or submitting a tax form, your users need to be able to communicate with you, and JavaScript allows them to do so;
+
+// send and receive data to and from external API's- harness the power of the thousands of useful API's that already exist or plug your frontend app into your own custom backend.
+
+// Get acces to the dom
+
+document.getElementsByTagName()
+document.getElementsByClassName()
+document.getSelection()
+document.getElementById()
